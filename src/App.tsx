2c99f1560060ac94d1b1cdef28ec7962d06d5a34
hifichevymane <blekbjek@gmail.com>
2023-08-11 20:07:48 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/esm/Button';
 
-function App() {
+const App: React.FC = () => {
+  const [videoUrl, setVideoUrl] = useState<string>();
+  const [isFileDownloaded, setIsFileDownloaded] = useState<boolean>(false);
+
+  // JSON 
+  interface VideoUrl {
+    url: string
+  }
+
+  // Set video url in input
+  const onSetVideoUrlHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setVideoUrl(event.target.value);
+  };
+
+  // Submit handler on submition the form
+  const submitFormHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
+      <h1 className={`${styles['font-default']}`}>YT-sounds.io</h1>
+      <h3 className={`${styles['font-default']}`}>Download your favorite video or music from YouTube!</h3>
+      <h4 className={`${styles['font-default']}`}>Paste video url to download</h4>
+      <form onSubmit={submitFormHandler} method="post">
+        <input type="url" onChange={onSetVideoUrlHandler} name="video-url-input" placeholder='Paste your url' />
+        <Button type='submit'>Submit</Button>
+      </form>
+      <a href="http://127.0.0.1:8000/uploads/test.mp3" target='blank' download='govno.txt'>Download</a>
+    </React.Fragment>
+  )
+};
 
 export default App;
