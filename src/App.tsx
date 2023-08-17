0f1ef09e80b8c7375a49c47c126cb57c46f4ca98
hifichevymane <blekbjek@gmail.com>
2023-08-17 13:24:53 +0300

    Add API requests to the backend

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,38 @@ import styles from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Button from 'react-bootstrap/esm/Button';
 
+// FastAPI API url link
+const API_URL: string = 'http://127.0.0.1:8000/api/';
+
 const App: React.FC = () => {
   const [videoUrl, setVideoUrl] = useState<string>();
-  const [isFileDownloaded, setIsFileDownloaded] = useState<boolean>(false);
-
-  // JSON 
-  interface VideoUrl {
-    url: string
-  }
+  // const [isFileDownloaded, setIsFileDownloaded] = useState<boolean>(false);
+  const [downloadedFileURL, setDownloadedFileURL] = useState<string>();
 
   // Set video url in input
   const onSetVideoUrlHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setVideoUrl(event.target.value);
+    console.log(videoUrl);
   };
 
   // Submit handler on submition the form
   const submitFormHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    // POST request to the API
+    fetch(`${API_URL}get_video/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        url: videoUrl
+      }),
+    }).then(res => {
+      return res.json()
+    })
+      .then(data => setDownloadedFileURL(data.file_path))
+      .catch(error => console.log(error));
   }
 
   return (
@@ -31,7 +46,7 @@ const App: React.FC = () => {
         <input type="url" onChange={onSetVideoUrlHandler} name="video-url-input" placeholder='Paste your url' />
         <Button type='submit'>Submit</Button>
       </form>
-      <a href="http://127.0.0.1:8000/uploads/test.mp3" target='blank' download='govno.txt'>Download</a>
+      {downloadedFileURL && <a href={downloadedFileURL} target='blank' download>Download</a>}
     </React.Fragment>
   )
 };
